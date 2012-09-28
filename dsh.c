6257011ec22442e5e2b5c287588ef437d141b4e6
Jay Wang <jayduke15@gmail.com>
2012-09-28 00:04:01 -0400

    Added Aleis' helper functions

--- dsh.c
+++ dsh.c
@@ -526,6 +526,49 @@ void eval(job_t *j){
 	
 	return;    
 }
+
+void put_job_in_foreground (job *j, int cont) {
+       /* Put the job into the foreground.  */
+       tcsetpgrp (shell_terminal, j->pgid);
+     
+       /* Send the job a continue signal, if necessary.  */
+       if (cont)
+         {
+           tcsetattr (shell_terminal, TCSADRAIN, &j->tmodes);
+           if (kill (- j->pgid, SIGCONT) < 0)
+             perror ("kill (SIGCONT)");
+         }
+     
+       /* Wait for it to report.  */
+       wait_for_job (j);
+     
+       /* Put the shell back in the foreground.  */
+       tcsetpgrp (shell_terminal, shell_pgid);
+     
+       /* Restore the shell's terminal modes.  */
+       tcgetattr (shell_terminal, &j->tmodes);
+       tcsetattr (shell_terminal, TCSADRAIN, &shell_tmodes);
+}
+
+/* Put a job in the background.  If the cont argument is true, send
+        the process group a SIGCONT signal to wake it up.  */
+     
+void put_job_in_background (job *j, int cont) {
+       /* Send the job a continue signal, if necessary.  */
+       if (cont)
+         if (kill (-j->pgid, SIGCONT) < 0)
+           perror ("kill (SIGCONT)");
+}
+
+void change_directory (job_t *j, int cont) {
+     //change directory
+     //I know he talked about this one being easy - can't remember what he said to do for it
+}
+
+void list_jobs (job_t *j, int cont) {
+     //print the jobs and their status
+}
+
 int main() {
 
 	init_shell();
